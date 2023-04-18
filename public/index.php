7f2e7f39282440a2c2cb10b5b9588c09442af622
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2023-04-18 10:59:33 -0300

    feat: checks the data provided by the controller and call views

--- public/index.php
+++ public/index.php
@@ -8,10 +8,18 @@ require_once './bootstrap.php';
 
 try {
     $router = new Router();
-
     $data = $router->router();
 
-    var_dump($data);
+    if (!check_data($data)) {
+        throw new Exception('Page not found.', 404);
+    }
+
+    extract($data['data']);
+
+    $view = $data['view'];
+    $css = $data['css'];
+
+    require VIEWS_PATH . '\\main_view.php';
 } catch (\Exception $e) {
     var_dump($e->getMessage(), $e->getCode());
 }
