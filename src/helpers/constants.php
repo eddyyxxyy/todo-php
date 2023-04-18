ec61e98f50787a345283b17b9af4b9fb2b0d8dad
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2023-04-18 11:02:46 -0300

    feat: defines LOGGED global named constant

--- src/helpers/constants.php
+++ src/helpers/constants.php
@@ -5,3 +5,4 @@ declare(strict_types=1);
 define("ROOT_PATH", dirname(__FILE__, 3));
 define("CONTROLLER_PATH", dirname(__FILE__, 2) . '\Controllers');
 define("VIEWS_PATH", dirname(__FILE__, 2) . '\views');
+define("LOGGED", "LOGGED");
