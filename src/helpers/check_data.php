df79736f658948aabd7d3284cc7ad830c7c47238
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2023-04-18 11:02:16 -0300

    feat: checks if all the controller data was passed properly

---
+++ src/helpers/check_data.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Checks if all the data needed to render views was properly passed.
+ *
+ * @param array $data View's data.
+ *
+ * @return boolean
+ */
+function check_data(array $data): bool
+{
+    if (
+        !isset($data['data']) ||
+        !isset($data['data']['title']) ||
+        !isset($data['view']) ||
+        !isset($data['css']) ||
+        !file_exists(VIEWS_PATH . "\\partials\\{$data['view']}")
+    ) {
+        return false;
+    }
+
+    return true;
+}
